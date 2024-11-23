97b2c6e4bbdc9da26931d0cf1508c4910ca91753
Aniket Kumar <aniketgmps@gmail.com>
2024-11-23 14:52:23 +0530

    re-edited the footer name

--- src/Components/common/Footer.jsx
+++ src/Components/common/Footer.jsx
@@ -173,7 +173,7 @@ const Footer = () => {
           </div>
 
           <div className="text-center">
-            Made by Archit Tanwar 🗿 © 2024 StudyNotion
+            Made by Aniket Kumar 🗿 © 2024 StudyNotion
           </div>
         </div>
       </div>
